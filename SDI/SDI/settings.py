4bd2882e23c65ff66fc75acd47ce08dd3d0e329c
Cristian Iorga <cristianus@DESKTOP-EV7BKVM>
2023-04-26 14:43:25 +0300

    postgresql server migration

--- SDI/SDI/settings.py
+++ SDI/SDI/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'SDI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

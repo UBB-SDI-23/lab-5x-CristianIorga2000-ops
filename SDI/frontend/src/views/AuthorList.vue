c24b9b0d709d4ce4c94fe2b0209b68b794fd1994
Cristian Iorga <cristianus@DESKTOP-EV7BKVM>
2023-04-26 14:17:11 +0300

    Lab 5 inclass

--- SDI/frontend/src/views/AuthorList.vue
+++ SDI/frontend/src/views/AuthorList.vue
@@ -1,56 +1,85 @@
 <template>
   <div>
     <h1>Authors</h1>
-    <ul v-if="authors">
-      <li v-for="(author, index) in authors" :key="author.id || index">
-        {{ author.name }}
-        <router-link :to="`/authors/${author.id}/edit`">Edit</router-link>
-        <button @click="deleteAuthor(author.id)">Delete</button>
-      </li>
-    </ul>
+    <DataTable v-if="authors" :value="authors" :sortField="sortField" :sortOrder="sortOrder" @sort="onSort">
+      <Column field="name" header="Name" sortable></Column>
+      <Column header="Actions">
+        <template #body="slotProps">
+          <router-link :to="`/authors/${slotProps.data.id}/edit`">Edit</router-link>
+          <button @click="deleteAuthor(slotProps.data.id)">Delete</button>
+        </template>
+      </Column>
+    </DataTable>
     <p v-else>Loading authors...</p>
   </div>
 </template>
 
+<script>
+import { DataTable, Column } from 'primevue/datatable';
+import api from '../services/api';
 
-  <script>
-  import api from '../services/api';
-  
-  export default {
-    name: 'AuthorList',
-    data() {
-      return {
-        authors: [],
-      };
+export default {
+  name: 'AuthorList',
+  components: {
+    DataTable,
+    Column,
+  },
+  data() {
+    return {
+      authors: [],
+      sortField: '',
+      sortOrder: 1,
+    };
+  },
+  methods: {
+    async fetchAuthors() {
+      try {
+        const response = await api.getAuthors();
+        this.authors = response.data.results;
+        console.log('Authors fetched:', this.authors);
+      } catch (error) {
+        console.error('Error fetching authors:', error);
+      }
     },
-    methods: {
-      async fetchAuthors() {
-        try {
-          const response = await api.getAuthors();
-          this.authors = response.data.results;
-          console.log('Authors fetched:', this.authors);
-        } catch (error) {
-          console.error('Error fetching authors:', error);
+
+    editAuthor(id) {
+      this.$router.push({ path: `/authors/edit/${id}` });
+    },
+
+    async deleteAuthor(id) {
+      try {
+        await api.deleteAuthor(id);
+        this.fetchAuthors();
+      } catch (error) {
+        console.error('Error deleting author:', error);
+      }
+    },
+
+    onSort(event) {
+      this.sortField = event.sortField;
+      this.sortOrder = event.sortOrder;
+      this.authors.sort((a, b) => {
+        let valueA = a[this.sortField];
+        let valueB = b[this.sortField];
+
+        if (typeof valueA === 'string') {
+          valueA = valueA.toLowerCase();
+          valueB = valueB.toLowerCase();
         }
-      },
-  
-      editAuthor(id) {
-        this.$router.push({ path: `/authors/edit/${id}` });
-      },
-  
-      async deleteAuthor(id) {
-        try {
-          await api.deleteAuthor(id);
-          this.fetchAuthors();
-        } catch (error) {
-          console.error('Error deleting author:', error);
+
+        if (valueA < valueB) {
+          return -1 * this.sortOrder;
+        } else if (valueA > valueB) {
+          return 1 * this.sortOrder;
+        } else {
+          return 0;
         }
-      },
+      });
     },
-    mounted() {
-      console.log("Real negro");
-      this.fetchAuthors();
-    },
-  };
-  </script>
-  
+  },
+  mounted() {
+    console.log('Mounted');
+    this.fetchAuthors();
+  },
+};
+</script>

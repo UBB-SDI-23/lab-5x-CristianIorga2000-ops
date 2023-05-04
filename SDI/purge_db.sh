38f364c5b69437996870650dbd707d7552800c21
Cristian Iorga <cristianus@DESKTOP-EV7BKVM>
2023-05-04 11:30:55 +0300

    db script

--- SDI/purge_db.sh
+++ SDI/purge_db.sh
@@ -15,9 +15,9 @@ BEGIN
    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE';
    END LOOP;
-   
+
    FOR r IN (SELECT column_name, column_default FROM information_schema.columns WHERE column_default LIKE 'nextval%' AND table_schema = current_schema()) LOOP
-       EXECUTE 'ALTER SEQUENCE ' || substring(r.column_default from '^nextval\(''(.*?)''\:.+') || ' RESTART WITH 1';
+       EXECUTE 'ALTER SEQUENCE ' || substring(r.column_default from 'nextval\(''([^'']+)') || ''' RESTART WITH 1';
    END LOOP;
 END \$\$;
 EOF

94f62ff00a19318474ab1e5217ccfde2419f3c60
Cristian Iorga <cristianus@DESKTOP-EV7BKVM>
2023-05-04 11:50:12 +0300

    db script v6

--- SDI/purge_db.sh
+++ SDI/purge_db.sh
@@ -6,6 +6,9 @@ export PGPASSWORD="123"
 export PGHOST="localhost"
 export PGDATABASE="mpi"
 
+# Terminate all connections to the database
+psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();"
+
 # Drop the existing database
 sudo dropdb --if-exists --username="$PGUSER" --host="$PGHOST" "$PGDATABASE"
 
